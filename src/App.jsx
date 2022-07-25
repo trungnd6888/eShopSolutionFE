a9db43ef92b5cc91107c21aadc0422cdf2f27da1
Trung Nguyen <trungk47s5@gmail.com>
2022-07-25 16:29:59 +0700

    setup axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { Routes, Route } from "react-router-dom"
 import Login from "./components/Login/Login"
 import './App.css'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+
+const theme = createTheme();
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
-
+      <ThemeProvider theme={theme}>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </ThemeProvider>
     </div>
   )
 }
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,16 +1,56 @@
-import { Avatar, Box, Container, CssBaseline, TextField, Typography } from '@mui/material';
-import React from 'react';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Checkbox from '@mui/material/Checkbox';
+import { Avatar, Box, Container, CssBaseline, TextField, Typography } from '@mui/material';
 import Button from '@mui/material/Button';
-import Link from '@mui/material/Link'
+import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
+import Link from '@mui/material/Link';
+import React, { useEffect } from 'react';
+import userApi from '../../api/UserApi';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
-Login.propTypes = {
+function Login() {
+    const schema = yup.object().shape({
+        username: yup.string().required("The username field is required"),
+        password: yup.string().required("The password field is required").min(6),
+        rememberMe: yup.bool()
+    });
 
-};
+    const form = useForm({
+        defaultValues: {
+            username: '',
+            password: '',
+            rememberMe: false
+        },
+        resolver: yupResolver(schema)
+    });
 
-function Login(props) {
+    const { register, handleSubmit, formState } = form;
+    const { errors, touchedFields } = formState;
+
+    const onSubmit = (values) => console.log('form values on submit: ', values);
+
+    console.log(errors, touchedFields);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const data = await userApi.login({
+                    username: null,
+                    password: null,
+                    rememberMe: true
+                });
+
+                console.log('Fetch data: ', data);
+            } catch (error) {
+                console.log("Fetch data is failed: ", error);
+            }
+        }
+
+        // fetchData();
+    }, []);
+
     return (
         <Container component="main" maxWidth="xs">
             <CssBaseline />
@@ -18,37 +58,40 @@ function Login(props) {
                 display: 'flex',
                 flexDirection: 'column',
                 alignItems: 'center',
-                marginTop: 9
-            }}>
-                <Avatar sx={{ bgcolor: 'primary.main' }}>
+                mt: 9
+            }} >
+                <Avatar sx={{ bgcolor: "primary.main" }}>
                     <LockOutlinedIcon />
                 </Avatar>
-                <Typography component="h1" variant="h5" sx={{ mt: 1 }}>
+                <Typography component="h1" variant="h5" sx={{ mt: 1 }} >
                     Sign in
                 </Typography>
-                <Box component="form" sx={{ mt: 3 }}>
+                <Box component="form" sx={{ mt: 3 }} onSubmit={handleSubmit(onSubmit)}>
                     <TextField
+                        {...register("username")}
                         variant="outlined"
                         label="Username"
-                        required
                         fullWidth
                         autoFocus
                         sx={{ mb: 3 }}
+                        error={!!errors.username}
+                        helperText={errors?.username?.message}
                     ></TextField>
                     <TextField
+                        {...register("password")}
                         type="password"
                         variant="outlined"
                         label="Password"
-                        required
                         fullWidth
                         sx={{ mb: 1 }}
+                        error={!!errors.password}
+                        helperText={errors?.password?.message}
                     ></TextField>
-
                     <FormControlLabel
-                        control={<Checkbox value="remember" color="primary" />}
+                        control={<Checkbox {...register("rememberMe")} color="primary" />}
                         label="Remember me"
                     />
-                    <Button variant="contained" fullWidth sx={{ mt: 3 }}>Sign in</Button>
+                    <Button type="submit" variant="contained" fullWidth sx={{ mt: 3 }}>Sign in</Button>
                     <Box sx={{
                         mt: 2.4,
                         display: 'flex',
@@ -66,7 +109,7 @@ function Login(props) {
                     &nbsp;2022.
                 </Typography>
             </Box>
-        </Container>
+        </Container >
     );
 }
 
